94254df35afa62a0a4f526e16c0f53941f4f81d3
Rien Maertens <maertensrien@gmail.com>
2017-07-29 14:52:27 +0200

    Add relative directory and some echo's to deployscript

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
+declare dir=$(pwd)
+
 declare timestamp=$(date +%Y-%m-%d_%H%M)
 
-declare release_dir="$HOME/releases/$timestamp"
+declare release_dir="$dir/releases/$timestamp"
+
 
 # Pull latest revision
+echo "-> Pulling latest release"
 git pull
 
 # Create release dir and copy sources to that dir
+echo "-> This release will be stored in $release_dir"
 mkdir -p "$release_dir"
 cp -ar src/* "$release_dir"
 
 # Link this relase to $current
-rm "$HOME/current"
-ln -sf "$HOME/releases/$timestamp" "$HOME/current"
+echo "-> Linking $dir/current to $release_dir"
+rm "$dir/current" || true # This may fail
+ln -sf "$dir/releases/$timestamp" "$dir/current"
+
+echo "-> Deployed succesfully!"
