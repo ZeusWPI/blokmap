f2220626e73c629cd56821b338bc274247f72185
redfast00 <redfast00@gmail.com>
2018-08-12 11:40:46 +0200

    Found cause of bug: up to date vs up-to-date

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ declare release_dir="$dir/releases/$timestamp"
 
 # Pull latest revision
 echo "-> Pulling latest release"
-git pull | grep 'Already up to date.' && echo "No updates, exiting..." && exit 0
+# Apparently, some systems put "up to date", while others put "up-to-date"
+git pull | grep -q 'Already up-?to-?date.' && echo "No updates, exiting..." && exit 20
 
 echo "-> Checking validity of data.json"
 jsonlint src/data.json
