dff357cfca5383e1c9ecf0737eae1e2815569e63
Midgard <midgard@zeus.ugent.be>
2020-05-20 17:33:11 +0200

    Update links to Zeus to zeus.gent

--- src/js/map.js
+++ src/js/map.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
     var map = L.map('map').setView([50.702, 4.335], 9);
     var osm = L.tileLayer('https://{s}.tiles.mapbox.com/v3/feliciaan.keoaj8d5/{z}/{x}/{y}{r}.png', {
-        attribution: 'Achtergrondkaart © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>-bijdragers (data) en © <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> (stijl) | <span lang="en">Made with ❤ by <a href="https://zeus.ugent.be">Zeus WPI</a></span>',
+        attribution: 'Achtergrondkaart © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>-bijdragers (data) en © <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> (stijl) | <span lang="en">Made with ❤ by <a href="https://zeus.gent/">Zeus WPI</a></span>',
         detectRetina: true
     }).addTo(map);
 
